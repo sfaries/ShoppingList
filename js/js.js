331adb2b8aefb2c2b2e234408dedfd2926a00bd4
sfaries <shaunfaries@gmail.com>
2015-04-28 13:02:08 -0400

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,14 @@
+
+/* 
+//jQuery Plugin for checked boxes by jk 
+$('.box').simpleImageCheck ({
+	image: 'unchecked.png',
+	imageChecked: 'check.png'
+	afterCheck: function (isChecked) {
+		if (isChecked) {
+			//do something
+		}
+	}
+})
+
+*/
