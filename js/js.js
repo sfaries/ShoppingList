b287f74fbca50cc203f6bcd7226706fee3bcb695
sfaries <shaunfaries@gmail.com>
2015-05-25 11:46:15 -0400

    Initial commit

--- js/js.js
+++ js/js.js
@@ -1,14 +1,46 @@
+$(document).ready(function(){
 
-/* 
-//jQuery Plugin for checked boxes by jk 
-$('.box').simpleImageCheck ({
-	image: 'unchecked.png',
-	imageChecked: 'check.png'
-	afterCheck: function (isChecked) {
-		if (isChecked) {
-			//do something
-		}
-	}
-})
+//variables for input markup
+	var $text = $('#textbox');
 
+//add button click to append shopping list
+		$('#addbutton').on('click', function(){
+			$("#shoppinglist").append('<li class = "items">' + '<input type = "checkbox" class = "box">' + $text.val() + '<button class = "delete"> X </button>' + '</li>');
+		
+
+//X button click to delete item
+
+		$('.delete').on('click', function(event){
+			$(event.target).closest("li").remove();
+			
+			});
+
+//checkbox click to strike shopinglist items using toggle
+
+		$('.box').on('click', function(event){
+			$(event.target).closest('li').toggleClass("itemsStrike");
+		
+			});
+	});
+
+//HARDCODED delete and strike
+/*
+		$('.box').on('click', function(event){
+			$(event.target).closest('li').toggleClass("itemsStrike");
+		
+			});
+
+		$('.delete').on('click', function(event){
+			$(event.target).closest("li").remove();
+			
+			});
+*/
+
+});
+
+//Alternate strike code
+/*
+$('li').on('click', function(){
+			$(this).toggleClass('itemsStrike');
+		});
 */
